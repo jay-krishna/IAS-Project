86cfe30dd17318fcd75902667899af9dcb934eaf
Gaurav Chaudhari <gaurav.chaudhari@students.iiit.ac.in>
2020-04-22 19:34:33 +0530

    Update validate.py

--- UI Complete/codes/validate.py
+++ UI Complete/codes/validate.py
@@ -1,6 +1,23 @@
-def auth(username,password):
+import json
+import requests
 
-	return True
+
+def auth(username,password):
+    resp = dict()
+    resp["username"] = username
+    resp["password"] = password
+    req = requests.post(url="http://0.0.0.0:5056/authlogin",data=json.dumps(resp))
+    req = json.loads(req.text)
+    if req["status"] == "failure":
+        return False
+    return True
 
 def signup(username,password):
-	x=1
+    resp = dict()
+    resp["username"] = username
+    resp["password"] = password
+    req = requests.post(url="http://0.0.0.0:5056/authsignup",data=json.dumps(resp))
+    req = json.loads(req.text)
+    if req["status"] == "failure":
+        return False
+    return True
