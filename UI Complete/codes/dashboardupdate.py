9e04b3cd58c07b08637645c2478b8a01fdf859bf
Gaurav Chaudhari <gaurav.chaudhari@students.iiit.ac.in>
2020-04-23 11:12:50 +0530

    Update dashboardupdate.py

--- UI Complete/codes/dashboardupdate.py
+++ UI Complete/codes/dashboardupdate.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 def update():
-	r=requests.get("http://127.0.0.1:7070/recv")
+	r=requests.get("http://127.0.0.1:5056/req")
 	data=r.json()
 	send_data=[]
+	print(data)
 	for app in data:
+		# print(app)
 		app_name=app["appname"]
 		for _ in app["data"]:
-			send_data.append([app_name,_["servicename"],_["status"],_["scheduled"],_["serviceid"],app_name+";"+_["serviceid"]])
+			# print(_)
+			send_data.append([app_name,_["servicename"],_["status"],_["scheduled"],_["serviceid"]])
 
 	return send_data
