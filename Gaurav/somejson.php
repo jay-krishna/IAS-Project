02c1b5cd3862cffa88e8c558bd88f37bb2e7b8bb
jay-krishna <jayk674@gmail.com>
2020-04-23 13:05:41 +0530

    Clean UP

--- Gaurav/somejson.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo 
-    "[{\"data\": [{\"serviceid\": \"demo\", \"servicename\": \"demo1\", \"status\": \"Running\", \"scheduled\": \"True\"}, {\"serviceid\": \"demo\", \"servicename\": \"demo2\", \"status\": \"Stopped\", \"scheduled\": \"False\"}, {\"serviceid\": \"demo\", \"servicename\": \"demo2\", \"status\": \"Running\", \"scheduled\": \"False\"}], \"appname\": \"App1\"}]"
-    ;
-?>
